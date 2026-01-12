f690bf4c45a39c11c8a4cfb0cc7c17bce72d50d2
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2026-01-12 22:31:10 +0900

    Add DFS implementation for island travel solution
    Implement DFS to calculate reachable areas in island travel.

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -1,3 +1,35 @@
+# dfs, bfs 다 가능
+
+import sys
+sys.setrecursionlimit(10001)
+
 def solution(maps):
+    drs, dcs = (-1, 1, 0, 0), (0, 0, -1, 1)
+    n, m = len(maps), len(maps[0])
+    
+    in_range = lambda r, c: 0 <= r < n and 0 <= c < m
+    
+    def dfs(r, c, visited):
+        count = int(maps[r][c])
+        visited[r][c] = flag
+        
+        for dr, dc in zip(drs, dcs):
+            nr, nc = r + dr, c + dc
+            if not in_range(nr, nc):
+                continue
+            if visited[nr][nc] == flag or maps[nr][nc] == 'X':
+                continue
+            count += dfs(nr, nc, visited)
+            
+        return count
+
+    visited = [[0] * m for _ in range(n)]
+    flag = 1
     answer = []
-    return answer
+    for i in range(n):
+        for j in range(m):
+            if maps[i][j] != 'X' and not visited[i][j]:
+                total = dfs(i, j, visited)
+                answer.append(total)
+            
+    return sorted(answer) if answer else [-1]
