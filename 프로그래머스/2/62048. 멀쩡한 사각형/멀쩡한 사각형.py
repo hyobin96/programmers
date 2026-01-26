8bb2dd5b930d59a8702eae88f7b35b63ecdbe526
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2026-01-26 23:44:57 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.01 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.py
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.py
@@ -1,25 +1,5 @@
-# h / w 는 열당 h / w 만큼 차지한다는 의미
-# h / w 만큼 올라가다가 열을 못넘어가면?
-# 5 / 3    10 / 3   15 / 3
-
 import math
 
 def solution(w,h):
-    answer = w * h
-    if w > h:
-        w, h = h, w
-        
-    나머지 = h % w
-    정수, 분자, 분모 = h // w, 나머지, w
-    cnt = 0
-    for _ in range(w):
-        cnt += 정수
-        if 분자:
-            cnt += 1
-        분자 += 나머지
-        if 분자 >= w:
-            cnt += 1
-            분자 %= w
-            
-            
-    return answer - cnt
+    return w * h - w - h + math.gcd(w, h)
+    
