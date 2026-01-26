01fb5c5f552fd36ce4fa7a12178ce53b19e640b8
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2026-01-26 23:55:32 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 5825.02 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.py
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.py
@@ -1,5 +1,20 @@
 import math
 
 def solution(w,h):
-    return w * h - w - h + math.gcd(w, h)
-    
+    answer = w * h
+    if w > h:
+        w, h = h, w
+        
+    나머지 = h % w
+    정수, 분자, 분모 = h // w, 나머지, w
+    cnt = 정수 * w
+    for _ in range(w):
+        if 분자:
+            cnt += 1
+        분자 += 나머지
+        if 분자 >= w:
+            cnt += 1
+            분자 %= w
+            
+            
+    return answer - cnt
