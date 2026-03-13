ad14bba0a2f00d204db085d21fbe42a7b18a35d3
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2026-03-14 00:37:08 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -0,0 +1,12 @@
+from math import ceil
+
+def solution(n,a,b):
+    cnt = 0
+    while a != b:
+        a = ceil(a / 2)
+        b = ceil(b / 2)
+        cnt += 1
+    
+    answer = cnt
+
+    return answer
