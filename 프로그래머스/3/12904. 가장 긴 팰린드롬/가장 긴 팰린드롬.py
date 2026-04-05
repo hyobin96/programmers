0916ec15bc341cdb73d07b93ff4483639f1d4bbe
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2026-04-05 22:32:03 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 5.40 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
@@ -3,12 +3,17 @@ def solution(s):
     s_length = len(s)
     A = [0] * s_length
     
+    r, center = -1, -1
     for i in range(s_length):
-        A[i] = 0
+        if r >= i:
+            A[i] = min(A[center - (i - center)], r - i)
         
         while i - A[i] - 1 >= 0 and i + A[i] + 1 < s_length and s[i - A[i] - 1] == s[i + A[i] + 1]:
             A[i] += 1
-        
+
+        if i + A[i] > r:
+            r, center = i + A[i], i
+            
     
     max_length = max(A)
     answer = max_length
