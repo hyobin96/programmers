e35f0e1bf1901e3260d17d87681d24f40164c961
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2026-06-04 22:50:59 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.12 ms, Memory: 62.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        int lux = 50, luy = 50, rdx = 0, rdy = 0;
+            
+        for (int i = 0; i < wallpaper.length; i++) {
+            for(int j = 0; j < wallpaper[i].length(); j++) {
+                if (wallpaper[i].charAt(j) == '#') {
+                    lux = Math.min(lux, i);
+                    luy = Math.min(luy, j);
+                    rdx = Math.max(rdx, i + 1);
+                    rdy = Math.max(rdy, j + 1);
+                }   
+            }
+        }
+        
+        
+        int[] answer = {lux, luy, rdx, rdy};
+        return answer;
+    }
+}
