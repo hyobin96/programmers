360126e4b571ca0552b15416f0d4d051d6816e30
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2026-06-07 23:31:17 +0900

    [level 2] Title: 가장 큰 수, Time: 99.32 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 113 MB, 시간: 137.35 ms
+메모리: 112 MB, 시간: 99.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 07일 23:17:26
+2026년 06월 07일 23:31:19
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -5,10 +5,9 @@ class Solution {
     public String solution(int[] numbers) {
         List<String> list = Arrays.stream(numbers)
             .mapToObj(String::valueOf)
+            .sorted((n1, n2) -> (n2 + n1).compareTo(n1 + n2))
             .collect(Collectors.toList());  
-        
-        list.sort((n1, n2) -> Long.valueOf(n2 + n1).compareTo(Long.valueOf(n1 + n2)));
-        
+                
         String answer = String.join("", list);
         
         return answer.charAt(0) == '0' ? "0" : answer;
